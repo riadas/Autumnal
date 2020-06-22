c0173804ca4557f641403e871db40892eddcbc8d
Ria Das <riadas@mit.edu>
2020-06-22 16:58:50 -0400

    compilation/GRID_SIZE updates working

--- app/resources/autumnmodels/AutumnModelsController.jl
+++ app/resources/autumnmodels/AutumnModelsController.jl
@@ -1,10 +1,30 @@
 module AutumnModelsController
 using Genie.Renderer.Html
+using Genie.Router
+using CausalDiscovery
 
 GRID_SIZE = 16;
+FORM_CONTENT = "";
 
 function autumnmodels()
-  html(:autumnmodels, :autumnmodelsdashboard, size=[1:GRID_SIZE*GRID_SIZE;])
+  println(length(FORM_CONTENT))
+  html(:autumnmodels, :autumnmodelsdashboard, size=[1:GRID_SIZE*GRID_SIZE;], content=FORM_CONTENT)
+end
+
+function compileautumn()
+  autumnString = @params(:autumnstring)
+  if autumnString != ""
+    parsedAutumn = eval(Meta.parse("au\"\"\"$(autumnString)\"\"\""))
+    compiledAutumn = compiletojulia(parsedAutumn)
+    mod = eval(compiledAutumn)
+
+    global GRID_SIZE = mod.GRID_SIZE
+    global FORM_CONTENT = showstring(parsedAutumn)
+  else
+    global GRID_SIZE = 16
+    global FORM_CONTENT = autumnString
+  end
+  autumnmodels()
 end
 
 end
--- routes.jl
+++ routes.jl
@@ -2,3 +2,5 @@ using Genie.Router
 using AutumnModelsController
 
 route("/", AutumnModelsController.autumnmodels)
+
+route("/compileautumn", AutumnModelsController.compileautumn, method = POST, named=:compile_autumn)
