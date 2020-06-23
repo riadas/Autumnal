0044a586cb25188d618a5ecbfa480a0bcad4acdf
Ria Das <riadas@mit.edu>
2020-06-23 17:55:55 -0400

    mvp

--- app/resources/autumnmodels/AutumnModelsController.jl
+++ app/resources/autumnmodels/AutumnModelsController.jl
@@ -11,25 +11,33 @@ MOD = nothing
 CLICK = nothing;
 PARTICLES = []
 ERROR = ""
+compiled = false
 running = false
+not_run_yet = true
 
 function autumnmodels()
   html(:autumnmodels, :autumnmodelsdashboard, 
        size=[0:GRID_SIZE*GRID_SIZE-1;], 
        content=FORM_CONTENT, 
        particles=map(particle -> GRID_SIZE*(particle.position.y) + particle.position.x, PARTICLES),
-       running=running)
+       running=running, compiled=compiled, not_run_yet=not_run_yet)
 end
 
 function compileautumn()
   autumnString = @params(:autumnstring)
   if autumnString != ""
+    try
       parsedAutumn = eval(Meta.parse("au\"\"\"$(autumnString)\"\"\""))
       compiledAutumn = compiletojulia(parsedAutumn)
       global MOD = eval(compiledAutumn)
-
+      global compiled = true
       global GRID_SIZE = MOD.GRID_SIZE
       global FORM_CONTENT = showstring(parsedAutumn)
+    catch ErrorException
+      global FORM_CONTENT = "" 
+      global GRID_SIZE = 16
+      global MOD = nothing
+    end
   else
     global GRID_SIZE = 16
     global FORM_CONTENT = autumnString
@@ -37,6 +45,8 @@ function compileautumn()
     global CLICK = nothing
     global PARTICLES = []
     global running = false
+    global compiled = false
+    global not_run_yet = true;
   end
   redirect(:get)
 end
@@ -54,13 +64,13 @@ function runautumn()
     MOD.init(nothing)
   end
   global running = true
+  global not_run_yet = false;
   redirect(:get)
 end
 
 function stopautumn()
   println("stopautumn")
-  global running = false
-  
+  global running = !running;
   redirect(:get)
 end
 
